a959f1e812cd7795f7bf0dcb46f7ec8a985acc10
Puiu Claudiu Madalin <madalin.c.puiu@gmail.com>
2020-03-19 21:57:12 +0200

    Modificare

--- lab1-master/src/loose/oose/fis/Main.java
+++ lab1-master/src/loose/oose/fis/Main.java
@@ -38,7 +38,7 @@ public class Main {
         pc2.adaugaProcesor(pc1);
         pc2.adaugaProcesor(c3);
 
-System.out.println("Buna ziua");
+System.out.println("Buna ziua1");
 
         System.out.println(pc2.proceseaza(documente));
     }
