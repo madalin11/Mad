0679cae94a9f9a8111a636f3cc6a139604d30237
Puiu Claudiu Madalin <madalin.c.puiu@gmail.com>
2020-03-19 19:32:12 +0200

    Adaugare afisare " Hello"

--- lab1-master/src/loose/oose/fis/Main.java
+++ lab1-master/src/loose/oose/fis/Main.java
@@ -39,6 +39,6 @@ public class Main {
         pc2.adaugaProcesor(c3);
 
         System.out.println(pc2.proceseaza(documente));
-        System.out.println("Hello");
+        System.out.println("Hello1");
     }
 }
